b4472e87ca0b244667bcabae78129d8d608b703c
Paal Aleksander Kaasa <paalaleks@hotmail.com>
2023-01-10 16:24:18 +0100

    initial

---
+++ src/api/sticker/controllers/sticker.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sticker controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sticker.sticker');
---
+++ src/api/sticker/services/sticker.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sticker service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::sticker.sticker');
